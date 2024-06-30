2c361627934a9ba9b4abed916df1f8d360eab313
SujunYao <58409387+SujunYao@users.noreply.github.com>
2024-06-30 23:52:04 +0800

    [dev] refine default screens

---
+++ src/components/prouct/consumption/ConsumptionCostContainer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import { withTheme } from "@beppla/tapas-ui";
+import ListHeader from "./ListHeader";
+import List from "./List";
+import PageInfo from "./PageInfo";
+
+const ConsumptionCostContainer: React.FC = ({ theme }: { theme: any }) => {
+  return (
+    <View style={styles.con}>
+      <ListHeader />
+      <List />
+      <PageInfo />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  con: {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+    overflow: "hidden",
+  },
+});
+
+export default withTheme(ConsumptionCostContainer);
+
---
+++ src/components/prouct/profit/ListHeader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+import { withTheme } from "@beppla/tapas-ui";
+
+const ListHeader: React.FC = ({ theme }: { theme: any }) => {
+  return <View></View>;
+};
+
+export default withTheme(ListHeader);
